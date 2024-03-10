e7e925950a6b065ba4076e3cfbb9810dbc783603
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-03-09 23:55:56 -0400

    Integrando typesript

--- monty-hall/src/pages/index.jsx
+++ monty-hall/src/pages/index.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react'
 import Presente from '@/components/Presente'
 import Porta from '@/components/Porta'
+import PortaModel from '@/model/PortaModel'
 
 export default function Home() {
-  return (
-    <div style={{display: 'flex'}}>
-        <Porta selecionada />
-        <Porta />
-    </div>
-  );
+    const [p1, setP1] = useState(new PortaModel(1))
+    const p2 = new PortaModel(2)
+
+    return (
+        <div style={{display: 'flex'}}>
+            <Porta porta={p1}/>
+            <Porta porta={p2}/>
+        </div>
+    );
 }
