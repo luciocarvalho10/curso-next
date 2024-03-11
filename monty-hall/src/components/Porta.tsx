abac1f670091d7c1fbb933a00caedf0974a8151c
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-03-10 21:09:16 -0400

    Adicionando eventos da porta

--- monty-hall/src/components/Porta.tsx
+++ monty-hall/src/components/Porta.tsx
@@ -3,19 +3,28 @@ import PortaModel from '@/model/PortaModel'
 
 interface PortaProps {
     porta: PortaModel
+    mudanca: (novaPorta: PortaModel) => void
 }
 
 export default function Porta(props: PortaProps) {
     const {porta} = props
-    const selecionada = porta?.selecionada ? styles.selecionada : ''
+    const selecionada = porta?.selecionada && !porta.aberta ? styles.selecionada : ''
+
+    const alterarSelecao = e => props.mudanca(porta.alterarSelecao())
+    const abrir = e => e.stopPropagation() && props.mudanca(porta.abrir())
+
+    const renderizarPorta = () => (
+        <div className={styles.porta}>
+            <div className={styles.numero}>{porta?.numero}</div>
+            <div className={styles.macaneta} onClick={abrir}></div>
+        </div>
+    )
+
     return (
-        <div className={styles.area}>
-            <div className={`${styles.estrutura} ${selecionada}`}>
-                <div className={styles.porta}>
-                    <div className={styles.numero}>{porta?.numero}</div>
-                    <div className={styles.macaneta}></div>
-                </div>
-            </div>
+        <div className={styles.area} onClick={alterarSelecao}>
+        <div className={`${styles.estrutura} ${selecionada}`}>
+            {porta.aberta ? false : renderizarPorta()}
+        </div>
             <div className={styles.chao}></div>
         </div>
     )
--- monty-hall/src/pages/index.tsx
+++ monty-hall/src/pages/index.tsx
@@ -5,12 +5,10 @@ import PortaModel from '@/model/PortaModel'
 
 export default function Home() {
     const [p1, setP1] = useState(new PortaModel(1))
-    const p2 = new PortaModel(2)
 
     return (
         <div style={{display: 'flex'}}>
-            <Porta porta={p1}/>
-            <Porta porta={p2}/>
+            <Porta porta={p1} mudanca={novaPorta => setP1(novaPorta)}/>
         </div>
     );
 }
