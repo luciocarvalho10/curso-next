772049e6716f522f37880adcd535dd53ed6b3865
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-03-12 20:47:29 -0400

    Refatorando parametor de funcao criarPorta

--- monty-hall/src/functions/portas.ts
+++ monty-hall/src/functions/portas.ts
@@ -1,9 +1,9 @@
 import PortaModel from '@/model/PortaModel'
 
-export function criarPortas(qtd: number, selecionanda: number): PortaModel[] {
+export function criarPortas(qtd: number, comPresente: number): PortaModel[] {
     return Array.from({ length: qtd }, (_, i) => {
         const numero = i + 1
-        const temPresente = numero === selecionanda
+        const temPresente = numero === comPresente
         return new PortaModel(numero, temPresente)
     })
 }
