19f6b8b46b0df4f48a724aa3f29f8ea88e76e43a
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-03-19 23:19:02 -0400

    Criando endpoint - questoes

--- quiz/src/pages/api/questoes/[id].ts
+++ quiz/src/pages/api/questoes/[id].ts
@@ -1,5 +1,11 @@
 import questoes from '@/pages/api/bancoDeQuestoes'
 
 export default function handler (req, res) {
-    res.status(200).json(questoes[0].paraObjeto())
+    const enviarQuestao = Q => res.status(200).json(Q.paraObjeto())
+
+    const id  = +req.query.id
+
+    const questao = questoes.filter(questao => questao.id === id)
+
+    questao.length === 1 ? enviarQuestao(questao[0]) : res.status(204).send()
 }
