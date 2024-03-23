47bace556d876a0680ea3545950de8c32d9d9420
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-03-23 19:37:48 -0400

    Refatornado

--- quiz/src/functions/arrays.ts
+++ quiz/src/functions/arrays.ts
@@ -1,3 +1,10 @@
+/**
+ * Embaralha os elementos de um array de números usando um algoritmo de ordenação aleatória.
+ *
+ * @param {number[]} array - O array de números a ser embaralhado.
+ * @returns {number[]} Um novo array com os elementos embaralhados.
+ */
+
 export function embaralhar(array: number[]):number[] {
     return array
         .map(valor => ({ valor, aleatorio: Math.random() }))
--- quiz/src/pages/api/questoes/[id].ts
+++ quiz/src/pages/api/questoes/[id].ts
@@ -1,13 +1,16 @@
-import MQuestao from "@/model/MQuestao"
+import { NextApiRequest, NextApiResponse } from 'next'
+import MQuestao from '@/model/MQuestao'
 import questoes from '@/pages/api/bancoDeQuestoes'
 
-export default function handler (req, res) {
-    const enviarQuestao = (Q: MQuestao) =>
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	const enviarQuestao = (Q: MQuestao) =>
 		res.status(200).json(Q.embaralharRespostas().paraObjeto())
 
-    const id  = +req.query.id
+	const id = Number(req.query.id)
 
-    const questao: MQuestao[] = questoes.filter(questao => questao.id === id)
+	const questao: MQuestao[] = questoes.filter(
+		questao => questao.getId() === id
+	)
 
-    questao.length === 1 ? enviarQuestao(questao[0]) : res.status(204).send()
+	questao.length === 1 ? enviarQuestao(questao[0]) : res.status(204).send//.send()
 }
