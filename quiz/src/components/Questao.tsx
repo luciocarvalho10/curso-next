7cfacb73b93e52000b6f2a90b1adaa100f96524c
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-03-25 21:32:38 -0400

    Renderizando resposta

--- quiz/src/components/Questao.tsx
+++ quiz/src/components/Questao.tsx
@@ -1,6 +1,7 @@
 import styles from '@/styles/Questao.module.css'
 import MQuestao from '@/model/MQuestao'
 import Enunciado from '@/components/Enunciado'
+import Resposta from '@/components/Resposta'
 
 interface IQuestaoProps {
     questao: MQuestao
@@ -9,9 +10,25 @@ interface IQuestaoProps {
 export default function Questao(props: IQuestaoProps) {
     const { questao } = props
 
+    function renderizarRespostas() {
+        return questao.getRespostas().map((resposta, i) => {
+            return (
+                <Resposta
+                    // key={`${questao.getId()}-${i}`}
+                    key={questao.getId() % i * Math.random()}
+                    resposta={resposta}
+                    indice={i}
+                    letra={String.fromCharCode(65 + i)}
+                    corFundoLetra='#F2C866'
+                />
+            )
+        })
+    }
+
     return (
         <div className={styles.main}>
             <Enunciado enunciado={questao.getEnunciado()} />
+            {renderizarRespostas()}
         </div>
     )
 }
