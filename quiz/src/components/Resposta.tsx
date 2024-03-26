d28c84e6f4d66da43ef89c3030685078c972f3ea
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-03-25 21:32:09 -0400

    Componente resposta e css

---
+++ quiz/src/components/Resposta.tsx
@@ -0,0 +1,28 @@
+import style from '@/styles/Resposta.module.css'
+import MResposta from '@/model/MResposta'
+
+interface IRespostaProps {
+    resposta: MResposta
+    indice: number
+    letra: string
+    corFundoLetra: string
+    // respostaFornecida: (indice: number) => void
+}
+
+export default function Resposta(props: IRespostaProps) {
+    return (
+		<div className={style.cartao}>
+			<div className={style.conteudo}>
+				<div className={style.frente}>
+					<div className={style.letra} style={{ backgroundColor: props.corFundoLetra }}>
+						{props.letra}
+					</div>
+					<div className={style.resposta}>
+						{props.resposta.getValor()}
+					</div>
+				</div>
+				<div className={style.verso}></div>
+			</div>
+		</div>
+	)
+}
