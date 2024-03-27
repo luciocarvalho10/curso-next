f3eaa926f91702f6347b466dfa6330265c8e88fd
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-03-27 01:02:30 -0400

    Refatorando

--- quiz/src/functions/arrays.ts
+++ quiz/src/functions/arrays.ts
@@ -1,12 +1,13 @@
-/**
- * Embaralha os elementos de um array de números usando um algoritmo de ordenação aleatória.
- *
- * @param {number[]} array - O array de números a ser embaralhado.
- * @returns {number[]} Um novo array com os elementos embaralhados.
- */
+import MResposta from "@/model/MResposta";
 
-export function embaralhar(array: number[]):number[] {
-    return array
+/**
+ * Embaralha os elementos de um array de respostas usando um algoritmo de ordenação aleatória.
+ *
+ * @param {MResposta[]} respostas - O array de respostas a ser embaralhado.
+ * @returns {MResposta[]} Um novo array com os elementos (respostas) embaralhados.
+ */
+export function embaralhar(respostas: MResposta[]): MResposta[] {
+    return respostas
         .map(valor => ({ valor, aleatorio: Math.random() }))
         .sort((a, b) => a.aleatorio - b.aleatorio)
         .map(objeto => objeto.valor)
