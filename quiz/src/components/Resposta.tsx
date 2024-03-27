51915fca08c6a702b2b7e80fb06bd1da88f04610
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-03-26 22:14:13 -0400

    acrescentando verso do card

--- quiz/src/components/Resposta.tsx
+++ quiz/src/components/Resposta.tsx
@@ -11,17 +11,40 @@ interface IRespostaProps {
 
 export default function Resposta(props: IRespostaProps) {
     return (
-		<div className={style.cartao} onClick={() => props.respostaFornecida(props.indice)} >
+		<div
+			className={style.cartao}
+			onClick={() => props.respostaFornecida(props.indice)}>
 			<div className={style.conteudo}>
-				<div className={style.frente}>
-					<div className={style.letra} style={{ backgroundColor: props.corFundoLetra }}>
-						{props.letra}
+				{!props.resposta.getRevelada() ? (
+					<div className={style.frente}>
+						<div
+							className={style.letra}
+							style={{ backgroundColor: props.corFundoLetra }}>
+							{props.letra}
+						</div>
+						<div className={style.resposta}>
+							{props.resposta.getValor()}
+						</div>
 					</div>
-					<div className={style.resposta}>
-						{props.resposta.getValor()}
+				) : (
+					<div className={style.verso}>
+						{props.resposta.getCerta() ? (
+							<div className={style.certa}>
+								<div>A reposta certa é...</div>
+								<div className={style.resposta}>
+									{props.resposta.getValor()}
+								</div>
+							</div>
+						) : (
+							<div className={style.errada}>
+								<div>A reposta informada está errada...</div>
+								<div className={style.resposta}>
+									{props.resposta.getValor()}
+								</div>
+							</div>
+						)}
 					</div>
-				</div>
-				<div className={style.verso}></div>
+				)}
 			</div>
 		</div>
 	)
