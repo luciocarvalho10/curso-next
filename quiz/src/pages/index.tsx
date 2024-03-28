a489ee7b6767d966cca9ca3f2dfc238201ec94ed
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-03-27 21:22:05 -0400

    Acessando as questoes via API

--- quiz/src/pages/index.tsx
+++ quiz/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Questionario from '@/components/Questionario'
 import MQuestao from '@/model/MQuestao'
 import MResposta from '@/model/MResposta'
@@ -10,9 +10,32 @@ const questaoExemplo = new MQuestao(203, 'Qual é o coletivo de cães?', [
     MResposta.certa('Matilha'),
 ])
 
+const BASE_URL = 'http://localhost:3000/api'
 
 export default function Quiz() {
-    const [questao, setQuestao] = useState(questaoExemplo)
+    const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([])
+    const [questao, setQuestao] = useState<MQuestao>(questaoExemplo)
+
+    async function carregarIdsDasQuestoes() {
+        const res = await fetch(`${BASE_URL}/questionario`)
+        const idsDasQuestoes = await res.json()
+        console.log(idsDasQuestoes)
+        setIdsDasQuestoes(idsDasQuestoes)
+    }
+
+    async function carregarQuestao(idQuestao: number) {
+        const res = await fetch(`${BASE_URL}/questoes/${idQuestao}`)
+        const obj = await res.json()
+        console.log(obj)
+    }
+
+    useEffect(() => {
+        carregarIdsDasQuestoes()
+    }, [])
+
+    useEffect(() => {
+        idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0])
+    },[idsDasQuestoes])
 
     function questaoResposndida(questao: MQuestao) {}
 
