c5a8f7ce816786d62a04c81403fb65ccc02e3695
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-03-29 15:33:02 -0400

    Usando componente Estatistica e estilizando resultado

--- quiz/src/pages/resultado.tsx
+++ quiz/src/pages/resultado.tsx
@@ -1,17 +1,38 @@
+import { useRouter } from 'next/router'
+import styles from '@/styles/Resultado.module.css'
+import Estatistica from '@/components/Estatistica'
 import Botao from '@/components/Botao'
 
 export default function Resultado() {
+    const router: any = useRouter()
+
+    const perguntas = +router.query.perguntas
+    const certas = +router.query.certas
+    const percentual = Math.round((certas / perguntas ) * 100)
+
     return (
-        <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100vh'
-        }}>
-            <h1>Parabéns!</h1>
-            {/* <span>Voce acertou 80% das questões</span> */}
-            <Botao href='/' texto='Reiniciar' />
-        </div>
-    )
+		<div className={styles.main}>
+			<h1>Resultado Final</h1>
+			<div className={styles.resultado}>
+				<Estatistica
+					texto='Perguntas'
+					valor={perguntas}
+				/>
+				<Estatistica
+					texto='Certas'
+					valor={certas}
+                    corFundo='#9cd2a4'
+				/>
+				<Estatistica
+					texto='Percentual'
+					valor={`${percentual}%`}
+                    corFundo='#de6a33'
+				/>
+			</div>
+			<Botao
+				href='/'
+				texto='Reiniciar'
+			/>
+		</div>
+	)
 }
