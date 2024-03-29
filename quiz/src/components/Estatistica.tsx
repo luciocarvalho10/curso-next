e98de811e412ebdc3412879f2b5bf12ca90c530d
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-03-29 15:32:17 -0400

    Criando componente Estatistica e css

---
+++ quiz/src/components/Estatistica.tsx
@@ -0,0 +1,28 @@
+import styles from '@/styles/Estatistica.module.css'
+
+interface IEstatisticaProps {
+    valor: string | number
+    texto: string
+    corFundo?: string
+    corFonte?: string
+}
+
+export default function Estatistica(props: IEstatisticaProps) {
+    return (
+		<div className={styles.main}>
+			<div
+				className={styles.valor}
+				style={{
+					backgroundColor: props.corFundo ?? '#FDD60F',
+					color: props.corFonte ?? '#333',
+				}}>
+				{props.valor}
+			</div>
+
+            <div className={styles.texto}>
+                <span>{props.texto}</span>
+            </div>
+
+		</div>
+	)
+}
