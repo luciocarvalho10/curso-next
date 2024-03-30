14275193e6000af8ab3e9a18a9adba7ce4876c51
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-03-29 22:37:39 -0400

    Aplicando animação filp

--- quiz/src/components/Resposta.tsx
+++ quiz/src/components/Resposta.tsx
@@ -2,49 +2,49 @@ import style from '@/styles/Resposta.module.css'
 import MResposta from '@/model/MResposta'
 
 interface IRespostaProps {
-    resposta: MResposta
-    indice: number
-    letra: string
-    corFundoLetra: string
-    respostaFornecida: (indice: number) => void
+	resposta: MResposta
+	indice: number
+	letra: string
+	corFundoLetra: string
+	respostaFornecida: (indice: number) => void
 }
-
 export default function Resposta(props: IRespostaProps) {
-    return (
+	const respostaRevelada = props.resposta.getRevelada()
+		? style.respostaRevelada
+		: ''
+
+	return (
 		<div
 			className={style.cartao}
 			onClick={() => props.respostaFornecida(props.indice)}>
-			<div className={style.conteudo}>
-				{!props.resposta.getRevelada() ? (
-					<div className={style.frente}>
-						<div
-							className={style.letra}
-							style={{ backgroundColor: props.corFundoLetra }}>
-							{props.letra}
-						</div>
-						<div className={style.resposta}>
-							{props.resposta.getValor()}
-						</div>
+			<div className={`${respostaRevelada} ${style.conteudo}`}>
+				<div className={style.frente}>
+					<div
+						className={style.letra}
+						style={{ backgroundColor: props.corFundoLetra }}>
+						{props.letra}
 					</div>
-				) : (
-					<div className={style.verso}>
-						{props.resposta.getCerta() ? (
-							<div className={style.certa}>
-								<div>A reposta certa é...</div>
-								<div className={style.resposta}>
-									{props.resposta.getValor()}
-								</div>
-							</div>
-						) : (
-							<div className={style.errada}>
-								<div>A reposta informada está errada...</div>
-								<div className={style.resposta}>
-									{props.resposta.getValor()}
-								</div>
-							</div>
-						)}
+					<div className={style.resposta}>
+						{props.resposta.getValor()}
 					</div>
-				)}
+				</div>
+				<div className={style.verso}>
+					{props.resposta.getCerta() ? (
+						<div className={style.certa}>
+							<div>A reposta certa é...</div>
+							<div className={style.resposta}>
+								{props.resposta.getValor()}
+							</div>
+						</div>
+					) : (
+						<div className={style.errada}>
+							<div>A reposta informada está errada...</div>
+							<div className={style.resposta}>
+								{props.resposta.getValor()}
+							</div>
+						</div>
+					)}
+				</div>
 			</div>
 		</div>
 	)
