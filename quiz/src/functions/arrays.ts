e0d80e43047b389f719fc32f6bbcac36b74fc554
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-03-29 22:36:18 -0400

    Refatorando - warn e error do build

--- quiz/src/functions/arrays.ts
+++ quiz/src/functions/arrays.ts
@@ -1,11 +1,11 @@
 /**
  * Embaralha os elementos de um array de números usando um algoritmo de ordenação aleatória.
  *
- * @param {number[]} array - O array de números a ser embaralhado.
- * @returns {number[]} Um novo array com os elementos embaralhados.
+ * @param {any[]} array - O array de números a ser embaralhado.
+ * @returns {any[]} Um novo array com os elementos embaralhados.
  */
 
-export function embaralhar(array: number[]):number[] {
+export function embaralhar(array: any[]):any[] {
     return array
         .map(valor => ({ valor, aleatorio: Math.random() }))
         .sort((a, b) => a.aleatorio - b.aleatorio)
--- quiz/src/pages/api/questionario/index.ts
+++ quiz/src/pages/api/questionario/index.ts
@@ -2,5 +2,6 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import questoes from '@/pages/api/bancoDeQuestoes'
 import { embaralhar } from '@/functions/arrays'
 
-export default (req: NextApiRequest, res: NextApiResponse) =>
-	res.status(200).json(embaralhar(questoes.map(q => q.getId())))
+export default function questionario(req: NextApiRequest, res: NextApiResponse){
+	return res.status(200).json(embaralhar(questoes.map(q => q.getId())))
+}
