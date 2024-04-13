cb5a169af0c33f12d00de964a1fac82bc16d3822
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-04-13 17:24:44 -0400

    Corrigindo identação

--- template-admin/src/components/template/Layout.tsx
+++ template-admin/src/components/template/Layout.tsx
@@ -3,17 +3,29 @@ import Cabecalho from '@/components/template/Cabecalho'
 import Conteudo from '@/components/template/Conteudo'
 
 interface ILayoutProps {
-    titulo: string
-    subtitulo: string
-    children?: any
+	children?: any
+	subtitulo: string
+	titulo: string
 }
 
 export default function Layout(props: ILayoutProps) {
-    return (
-        <div>
-            <MenuLateral />
-            <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
-            <Conteudo>{props.children}</Conteudo>
-        </div>
-    )
+	return (
+		<div
+			className={`
+            flex h-screen w-screen
+        `}>
+			<MenuLateral />
+			<div
+				className={`
+                flex flex-col w-full p-7
+                bg-gray-300 dark:bg-gray-800
+            `}>
+				<Cabecalho
+					titulo={props.titulo}
+					subtitulo={props.subtitulo}
+				/>
+				<Conteudo>{props.children}</Conteudo>
+			</div>
+		</div>
+	)
 }
