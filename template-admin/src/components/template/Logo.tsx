1fbe94a4316142a13bf490570580aa4b8d68242d
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-04-14 12:29:23 -0400

    refatorando componente logo

--- template-admin/src/components/template/Logo.tsx
+++ template-admin/src/components/template/Logo.tsx
@@ -2,17 +2,10 @@ import logo from '../../../public/logo.png'
 
 export default function Logo() {
 	return (
-		<div
-			className={`
-                flex flex-col items-center justify-center
-                bg-gradient-to-r from-indigo-500 to-purple-800
-                w-20 h-20
-            `}>
-			<img
-				src={logo.src}
-				alt='logo'
-				className={`flex w-14 h-14 `}
-			/>
-		</div>
+		<img
+			src={logo.src}
+			alt='Na Finha Fáquina Funciona'
+			className={`flex w-14 h-14 `}
+		/>
 	)
 }
