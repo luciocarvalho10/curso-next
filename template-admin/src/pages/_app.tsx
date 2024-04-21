4894662014e664b2babc030ae4ed52d2b2836758
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-04-21 03:50:59 -0400

    Dispondo componente provider AuthProvider

--- template-admin/src/pages/_app.tsx
+++ template-admin/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import { AppProvider } from "@/data/context/AppContext";
+import { AuthProvider } from "@/data/context/AuthContext";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-		<AppProvider>
-			<Component {...pageProps} />
-		</AppProvider>
+		<AuthProvider>
+			<AppProvider>
+				<Component {...pageProps} />
+			</AppProvider>
+		</AuthProvider>
   )
 }
