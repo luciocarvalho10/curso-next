ab61748c1c06e73cc414c64e1063e5f93aba6c12
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-04-21 01:42:52 -0400

    Inicializando o firebase

---
+++ template-admin/src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+
+const firebaseConfig = {
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+}
+
+if (!firebase.apps.length) {
+	firebase.initializeApp(firebaseConfig)
+}
+
+export default firebase
