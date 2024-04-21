423c72a17d2644565871b8b0d308685ab3314a68
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-04-21 15:33:51 -0400

    pagina de perfil do usuario

---
+++ template-admin/src/pages/perfil.tsx
@@ -0,0 +1,11 @@
+import Layout from '@/components/template/Layout'
+
+export default function Perfil() {
+	return (
+		<Layout
+			titulo='Perfil do Usuário'
+			subtitulo='Administre os seus dados de Usuário'>
+			<h3>Perfil do Usuário</h3>
+		</Layout>
+	)
+}
