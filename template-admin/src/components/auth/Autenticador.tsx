0b8dd90b44f665cbedc410c45f1cedbd940e825f
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-04-22 22:23:40 -0400

    Acrescentando camada de protecao para aplicacao

--- template-admin/src/components/auth/Autenticador.tsx
+++ template-admin/src/components/auth/Autenticador.tsx
@@ -2,12 +2,28 @@ import Image from 'next/image'
 import loading from '../../../public/images/loading.gif'
 import useAuth from '@/data/hook/UseAuth'
 import router from 'next/router'
+import Head from 'next/head'
 
 export default function Autenticador(props: any) {
 	const { usuario, carregando } = useAuth()
 
 	function renderizarConteudo() {
-		return <>{props.children}</>
+		return (
+            <>
+                <Head>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                if(!document.cookie?.includes("admin-template-auth")) {
+                                    window.location.href = "/autenticacao"
+                                }
+                            `,
+                        }}
+                    />
+                </Head>
+                {props.children}
+            </>
+        )
 	}
 
 	function renderizarCarregando() {
