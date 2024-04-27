8959dc6c224c6ce666b8aba74f42427a3f6af350
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-04-26 23:21:22 -0400

    classe Cliente

---
+++ next-crud/src/core/Cliente.ts
@@ -0,0 +1,32 @@
+export default class Cliente {
+    #id: string | null
+    #nome: string
+    #idade: number
+
+
+    constructor(
+        nome: string,
+        idade: number,
+        id: string | null = null
+    ) {
+        this.#nome = nome
+        this.#idade = idade
+        this.#id = id
+    }
+
+    static vazio() {
+        return new Cliente('', 0)
+    }
+
+    get id(): string | null {
+        return this.#id
+    }
+
+    get nome(): string {
+        return this.#nome
+    }
+
+    get idade(): number {
+        return this.#idade
+    }
+}
