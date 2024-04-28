f20995ed5203e9868eec2b41c6324d02351661f8
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-04-28 14:29:00 -0400

    Criando componente Botao

---
+++ next-crud/src/components/Botao.tsx
@@ -0,0 +1,18 @@
+interface IBotaoProps {
+    cor?: 'green' | 'blue' | 'gray'
+    className?: string
+    children: any
+}
+
+export default function Botao(props: IBotaoProps) {
+    const cor = props.cor ?? 'gray'
+    return (
+        <button className={`
+            bg-gradient-to-r from-${cor}-400 to-${cor}-700
+            text-white px-4 py-2 rounded-md
+            ${props.className}
+        `}>
+            {props.children}
+        </button>
+    )
+}
