079a7e9191e10ec133a2cca3043bb2c2e3a038be
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-04-28 16:16:20 -0400

    Criando formulario e usando componente entrada

---
+++ next-crud/src/components/Formulario.tsx
@@ -0,0 +1,52 @@
+import Entrada from './Entrada'
+import { useState } from 'react'
+import Botao from './Botao'
+import Cliente from '@/core/Cliente'
+
+interface IFormularioProps {
+    cliente: Cliente
+}
+
+export default function Formulario(props: IFormularioProps) {
+    const id = props.cliente?.id
+
+	const [nome, setNome] = useState(props.cliente?.nome ?? '')
+	const [idade, setIdade] = useState(props.cliente?.idade ?? 0)
+
+	return (
+		<div
+			className={`
+            bg-gray-300 rounded-md px-8 pt-6 pb-6 mb-2
+        `}>
+			<form>
+				{id && (
+					<Entrada
+						texto='ID'
+						valor={id}
+                        className='mb-4'
+					/>
+				)}
+				<Entrada
+					texto='Nome'
+					valor={nome}
+                    valorMudou={setNome}
+                    className='mb-4'
+				/>
+				<Entrada
+					texto='Idade'
+					tipo='number'
+					valor={idade}
+                    valorMudou={setIdade}
+				/>
+			</form>
+			<div className='flex justify-end mt-6'>
+				<Botao
+					cor={id ? 'green' : 'blue'}
+					className='mr-2'>
+					{ id ? 'Alterar' : 'Salvar' }
+				</Botao>
+				<Botao>Cancelar</Botao>
+			</div>
+		</div>
+	)
+}
